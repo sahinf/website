0c3fdb4e9ebc29a9c93882fec07883c2f0b86345
sahinf <furkansahin824@gmail.com>
2022-08-19 04:50:59 +0000

    Deploying to gh-pages from @ sahinf/website@d7daf4769a0682118a73121b48cbf4e5a90c0669 🚀

---
+++ _app/immutable/chunks/0-6eff01a1.js
@@ -0,0 +1,1 @@
+import{default as m}from"../components/layout.svelte-e105e7df.js";import"./index-df6a2127.js";export{m as component};
---
+++ _app/immutable/chunks/1-ca9c5b7c.js
@@ -0,0 +1,1 @@
+import{default as m}from"../components/error.svelte-6f84932b.js";import"./index-df6a2127.js";export{m as component};
