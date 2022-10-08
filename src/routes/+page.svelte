1e8f149503828bfb574f415d5521da4f5575d8cd
furkan <furkansahin824@gmail.com>
2022-10-08 16:23:37 -0500

    ``test`: testing whether dns A NAME rerouting actually works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Init Furkan Website</h1>
+<!-- <h1>Init Furkan Website</h1> -->
+<img src="lord-farquad-furkan.bmp" alt="" />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
